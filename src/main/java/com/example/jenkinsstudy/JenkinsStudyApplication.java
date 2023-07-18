d953ed3743c2e46a969c4fd1db5a33757fb16925
정핸모 <gusah009@naver.com>
2023-07-18 20:44:23 +0900

    init java sample project

---
+++ src/main/java/com/example/jenkinsstudy/JenkinsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsStudyApplication.class, args);
+    }
+
+}

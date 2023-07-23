8303c6b9d81bae4076400287371f98c046226925
정핸모 <gusah009@naver.com>
2023-07-23 09:27:30 +0900

    개행 수정

--- src/main/java/com/example/jenkinsstudy/SampleComponent.java
+++ src/main/java/com/example/jenkinsstudy/SampleComponent.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SampleComponent implements InitializingBean {
-    
+
     @Override
     public void afterPropertiesSet() {
         System.out.println("컴포넌트 스캔 당하는중~~~");

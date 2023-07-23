31cfa5905a1165683935e3d45ad5c3b9d7bcf06e
정핸모 <gusah009@naver.com>
2023-07-23 09:04:33 +0900

    컴포넌트 추가~!

---
+++ src/main/java/com/example/jenkinsstudy/SampleComponent.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsstudy;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleComponent implements InitializingBean {
+    
+    @Override
+    public void afterPropertiesSet() {
+        System.out.println("컴포넌트 스캔 당하는중~~~");
+    }
+}

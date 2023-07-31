39b561e7c12dd52b57545384c1a67688bcfa4623
정핸모 <gusah009@naver.com>
2023-07-31 18:05:25 +0900

    hi 컨트롤러 수정

--- src/main/java/com/example/jenkinsstudy/SimpleController.java
+++ src/main/java/com/example/jenkinsstudy/SimpleController.java
@@ -1,13 +1,14 @@
 package com.example.jenkinsstudy;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
 
     @GetMapping
-    public String hi() {
-        return "hi";
+    public String hi(@RequestParam String name) {
+        return "hi + " + name;
     }
 }
